eabd9152c5b3ab45b5c4f25db423073e8025d954
Juan Jose Taboada <juanjo.fra@gmail.com>
2021-02-22 16:18:29 -0300

    Se agrego el logo para el loader y se agregaron los enlaces internos

--- js/custom.js
+++ js/custom.js
@@ -201,4 +201,16 @@ $(window).scroll(function(){
 
 	
 	
-	
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+      var destino = $(this.hash);
+      if (destino.length == 0) {
+        destino = $('a[name="' + this.hash.substr(1) + '"]');
+      }
+      if (destino.length == 0) {
+        destino = $('html');
+      }
+      $('html, body').animate({ scrollTop: destino.offset().top }, 1000);
+      return false;
+    });
+  });
